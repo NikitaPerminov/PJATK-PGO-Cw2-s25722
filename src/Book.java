efc63b5e089635a9137c1ac29f9f58528119e7a5
Никита Перминов <nikitafootball124@gmail.com>
2026-04-05 20:38:21 +0200

    First commit after creation of Book class + constructor with methods

---
+++ src/Book.java
@@ -0,0 +1,81 @@
+public class Book {
+
+    private String title;
+    private String author;
+    private int pages;
+    private boolean isAvailable;
+
+    public Book(String title, String author, int pages, boolean isAvailable) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if (title == null || title.isEmpty()) {
+
+            throw new IllegalArgumentException("Title cannot be null or empty.");
+
+        }
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        if (author == null || author.isEmpty()) {
+
+            throw new IllegalArgumentException("Author cannot be null or empty.");
+
+        }
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        if (pages <= 0) {
+
+            throw new IllegalArgumentException("Pages cannot be less than or equal to 0.");
+
+        }
+        this.pages = pages;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public void borrowBook(){
+        if(!isAvailable) {
+            throw new IllegalStateException("Book is already borrowed.");
+        }
+    }
+
+    public void returnBook(){
+        if(isAvailable){
+            throw new IllegalStateException("Book is already available.");
+        }
+        isAvailable = true;
+    }
+
+    public void showDetails(){
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available: " + isAvailable);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,25 @@
 public class Main {
     public static void main(String[] args){
 
+    Book book = new Book("The Hobbit", "Nikita P", 78, true);
 
+    book.showDetails();
+    book.borrowBook();
+
+    System.out.println("Book borrowed.");
+    System.out.println("Thank you for using our service.");
+        System.out.println("\t");
+
+    book.setAvailable(!book.isAvailable());
+    book.showDetails();
+        System.out.println("\t");
+
+    //book.borrowBook();
+
+    book.returnBook();
+    book.showDetails();
+    System.out.println("Book returned.");
+    System.out.println("Thank you for using our service.");
 
     }
 
